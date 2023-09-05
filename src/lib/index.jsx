d482f1c36319a046e6508fa8d3cc0942c1de8253
johary1 <joharisoa@gmail.com>
2023-09-05 16:09:22 +0200

    rename import

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import MyModal from "./ModalComponent";
 import "./ModalComponent.css";
-const MyCustomModal = () => {
+const ModalComponent = () => {
     const [showModal, setShowModal] = useState(false);
     const handleShowModal = () => {
       setShowModal(true);
@@ -14,10 +14,10 @@ const MyCustomModal = () => {
         <header className="App-header">
           <h1>Customizable Modal Component Example</h1>
           <button onClick={handleShowModal}>Show Modal</button>
-          <MyCustomModal show={showModal} handleClose={handleCloseModal} />
+          <MyModal show={showModal} handleClose={handleCloseModal} />
         </header>
       </div>
     );
 };
 
-export default MyModal;
+export default ModalComponent;
