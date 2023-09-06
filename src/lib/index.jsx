f5395a4c1ae795a009b94a292a4d08054e9597b4
johary1 <joharisoa@gmail.com>
2023-09-06 11:14:53 +0200

    fix import from index file

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,23 +1,3 @@
-import { useState } from "react";
 import MyModal from "./ModalComponent";
-import "./ModalComponent.css";
-const ModalComponent = () => {
-    const [showModal, setShowModal] = useState(false);
-    const handleShowModal = () => {
-      setShowModal(true);
-    };
-    const handleCloseModal = () => {
-      setShowModal(false);
-    };
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Customizable Modal Component Example</h1>
-          <button onClick={handleShowModal}>Show Modal</button>
-          <MyModal show={showModal} handleClose={handleCloseModal} />
-        </header>
-      </div>
-    );
-};
 
-export default ModalComponent;
+export default MyModal ;
